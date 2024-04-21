183763e7e55b57e841ac0901564e6d5e397e6e0a
filipemilk <filipeleche@gmail.com>
2024-04-21 11:36:22 -0300

    Mais 1

--- processo/consulta.js
+++ processo/consulta.js
@@ -11,15 +11,93 @@ var a = window.document.getElementById('area')
 var d = window.document.getElementById('dados')
 var arq = window.document.getElementById('arquivo')
 
+var selecao = window.document.getElementById('iautordata') 
+var autor = window.document.getElementById('inome')
+
+var login = window.document.getElementById('ilogin')
+var senha = window.document.getElementById('isenha')
+
+var btnConsulta = document.getElementById('but-consulta')
+
+//________________________________________________________________________________
+
+function entrar() {
+  if ((login.value == "flpsystem" && senha.value == "abracadabra40") || (login.value == "admin" && senha.value == "admin")) {
+    window.location.href = "processo.html"
+  } else {
+    window.alert('Usuário ou senha inválido!')
+  }
+}
+
 //________________________________________________________________________________
 
 document.addEventListener('keypress', function(e) {
   if(e.key === "Enter") {
-    const btn = document.getElementById('but')
-    btn.click()
+    btnConsulta.click()
     e.preventDefault() //Parar o evento.
   }
 })
+//________________________________________________________________________________
+
+function desabilitar() {
+  if (selecao.value == "Autor") {
+    autor.removeAttribute('disabled')
+    myDate.setAttribute('disabled', '')
+    myDate.value = ''
+  } else {
+    autor.setAttribute('disabled', '')
+    myDate.removeAttribute('disabled')
+    autor.value = ''
+    dataHoje()
+  }
+}
+
+selecao.addEventListener('input', desabilitar)
+
+//________________________________________________________________________________
+
+function botaoVerde() {
+  btnConsulta.style.background = 'green'
+  btnConsulta.value = "Consultando..."
+  btnConsulta.setAttribute('disabled', '')
+
+  btnConsulta.addEventListener('mouseover', function() {
+    btnConsulta.style.background = 'green'
+  })
+
+  btnConsulta.addEventListener('mouseout', function() {
+    btnConsulta.style.background = 'green'
+  })
+}
+//________________________________________________________________________________
+
+function botaoAzul() {
+  btnConsulta.style.background = 'rgb(79, 79, 209)'
+  btnConsulta.value = "Consultar"
+  btnConsulta.removeAttribute('disabled')
+
+  btnConsulta.addEventListener('mouseover', function() {
+    btnConsulta.style.background = 'rgb(64, 64, 168)'
+  })
+
+  btnConsulta.addEventListener('mouseout', function() {
+    btnConsulta.style.background = 'rgb(79, 79, 209)'
+  })
+}
+//________________________________________________________________________________
+
+function letraMaiuscula() {
+  var palavra = autor.value.split(' ')
+
+  for (let i in palavra) {
+    if (palavra[i].length <= 3 && (palavra[i].charAt(0) == 'd' || palavra[i].charAt(0) == 'D')) { //Ex. de, dos, das...
+      palavra[i] = palavra[i].toLowerCase()
+    } else {
+      palavra[i] = palavra[i].charAt(0).toUpperCase() + palavra[i].slice(1).toLowerCase()
+    }
+  }
+  nome = palavra.join(' ')
+}
 
 //________________________________________________________________________________
 
@@ -52,6 +130,7 @@ function abrirArquivo() {
     .then(text => {
       const lista = text.split('\n')
       const filtrado = lista.filter(x => x.substring(0,4) === 'http')      //O método trim() remove os espaços das tags vazias.
+      letraMaiuscula()
       consultar(filtrado)
     })
     .catch(error => {
@@ -62,12 +141,20 @@ function abrirArquivo() {
 
 function consultar(sites) {
 
-  var palavraChave = myDate.value.split('-').reverse().join('/')
+  var palavraChave
+
+  if (autor.disabled == true) {
+    palavraChave = myDate.value.split('-').reverse().join('/')
+  } else {
+    palavraChave = nome
+  }
+
   var siteAberto = 0
   var item = 0
 
-  if (myDate.value != '') {
-    d.style.borderBottom = '1px solid #000'
+  if (palavraChave != '') {
+    botaoVerde()
+    res.style.borderTop = '1px solid #000'
     res.innerHTML = `<p>Total de Processos: ${sites.length}</p>`
     sites.forEach(site => {
       fetch(site)
@@ -75,9 +162,8 @@ function consultar(sites) {
         .then(text => {
           //Verificador de sites repetidos!
           if(sites.indexOf(sites[item]) != item) {
-            res.innerHTML += `<p>Endereço repetido:</p> ${sites[item]}`
-            a.style.maxWidth = '1000px'
-            d.style.textAlign = 'center'
+            res.innerHTML += `<p>Endereço repetido:</p> <p>${sites[item]}</p>`
+            botaoAzul()
           }
           item++
 
@@ -86,18 +172,26 @@ function consultar(sites) {
             window.open(site, '_blank') // Abre o site em uma nova aba
             siteAberto++
             window.alert(`Total de sites abertos: ${siteAberto}`)
+            botaoAzul()
           } else if (siteAberto == 0 && item == sites.length) {
             // console.log(`A palavra "${palavraChave}" não foi encontrada no site ${site}.`);
-            window.alert(`Não há processo tramitado nesta data: ${palavraChave}.`)
+            if (myDate.disabled == true) {
+              window.alert(`Nenhum processo encontrado com este nome: ${palavraChave}`)
+              botaoAzul()
+            } else {
+              window.alert(`Não há processo tramitado nesta data: ${palavraChave}`)
+              botaoAzul()
+            } 
           }
         })
         .catch(error => {
           //console.error(`Ocorreu um erro ao acessar o site ${site}:`, error);
-          res.innerHTML += `<p>O endereço de site está inválido:</p> ${site}`
-          a.style.maxWidth = '1000px'
-          d.style.textAlign = 'center'
+          res.innerHTML += `<p>O endereço de site está inválido:</p> <p>${site}</p>`
+          botaoAzul()
         });
     })
+  } else if (myDate.disabled == true) {
+    window.alert(`Insira um nome!`)
   } else {
     window.alert(`Insira uma data válida!`)
   }
